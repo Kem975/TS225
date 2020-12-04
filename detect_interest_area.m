ce9acd9413a16af60f66e670fb2110a6ee567fa8
ESCOBOSA Valentin <Valentin.Escobosa@bordeaux-inp.fr>
2020-12-04 20:14:38 +0100

    Surfruise

--- detect_interest_area.m
+++ detect_interest_area.m
@@ -1,54 +1,46 @@
-function [] = detect_interest_area(img_Y)
+function [D] = detect_interest_area(img_Y)
     
     [h,w] = size(img_Y);
 
     %Canny
-    sigma_g = 2.5;
+    sigma_g = h*w/100;
     N_canny = fix(3*sigma_g);
     [X, Y] = meshgrid(-N_canny:N_canny);
-    canny_x = -X./(2*pi*sigma_g^4)*exp(-(X.^2+Y.^2)/(2*sigma_g^2));
-    canny_y = -Y./(2*pi*sigma_g^4)*exp(-(X.^2+Y.^2)/(2*sigma_g^2));
-    
-    grad_x= conv2(img_Y,canny_x,'same');
+    canny_x = -X/(2*pi*sigma_g^4).*exp(-(X.^2+Y.^2)/(2*sigma_g^2));
+    canny_y = -Y/(2*pi*sigma_g^4).*exp(-(X.^2+Y.^2)/(2*sigma_g^2));
+    9
+    grad_x = conv2(img_Y,canny_x,'same');
+    10
     grad_y = conv2(img_Y,canny_y,'same');
-    
+        
     norm_grad = sqrt(grad_x.^2+grad_y.^2);
     
     grad_x = grad_x./norm_grad;
     grad_y = grad_y./norm_grad;
-    
+    11
     %Gaussien
-    sigma_t = 2;
+    sigma_t = min(h,w)/16;
     N_gauss = fix(3*sigma_t);
     [X,Y] = meshgrid(-N_gauss:N_gauss);
-    gaussien = -1/(2*pi*sigma_t^2).*exp(-(X.^2+Y.^2)/(2*sigma_t^2));    
-    W = conv2(img_Y,gaussien,'same');
+    W = 1/(2*pi*sigma_t^2).*exp(-(X.^2+Y.^2)/(2*sigma_t^2));  
+    12
     
     %Tenseur
-     T = zeros(h,w,3);
-%     for x=1:h
-%         for y=1:w
-%             for u=1:h
-%                 for v=1:w
-%                     if(x+u)<=h && (y+v)<=w
-%                         T(x,y,1)=T(x,y,1)+W(u,v)*grad_x(x+u,y+v)*grad_x(x+u,y+v);
-%                         T(x,y,2) = T(x,y,2)+W(u,v)*grad_x(x+u,y+v)*grad_y(x+u,y+v);
-%                         T(x,y,4)=T(x,y,4)+W(u,v)*grad_y(x+u,y+v)*grad_y(x+u,y+v);
-%                     end
-%                 end
-%             end
-%             T(x,y,3)=T(x,y,2);
-%         end
-%     end
+    T = zeros(h,w,3);
+    13
     grad_xy = grad_x.*grad_y;
+    14
     grad_xx = grad_x.*grad_x;
+    15
     grad_yy = grad_y.*grad_y;
+    16
     T(:,:,1)=conv2(grad_xx,W,'same');
+    17
     T(:,:,2) = conv2(grad_xy,W,'same');
+    18
     T(:,:,3) = conv2(grad_yy,W,'same');
+    19
     D = sqrt((T(:,:,1)-T(:,:,3)).^2+4*T(:,:,2).^2)./(T(:,:,1)+T(:,:,3));
     
-    figure,
-    imshow(uint8(abs(D)));
 end
 
--- region_interet.m
+++ region_interet.m
@@ -1,9 +1,22 @@
-clear;clc;close all
+clear;clc;
 
 addpath('codes_barres_img/');
-img = double(imread('code_barre_bouteille.jpg'));
+img = double(imread('moyen2.png'));
+[h,w] = size(img);
+
+%img = imresize(img, 1);
 
 img_Y = 0.229*img(:,:,1)+0.587*img(:,:,2)+0.114*img(:,:,3);
 
-detect_interest_area(img_Y);
+D = detect_interest_area(img_Y);
 
+%%
+D_hist = hist(D,10000);
+[k,idx] = otsu(D_hist,10000); 
+masque =D> idx/10000;
+%masque = abs(masque-1);
+figure,
+subplot(1,2,1)
+imshow(uint8(img_Y));
+subplot(1,2,2)
+imshow(uint8(masque.*img_Y));
